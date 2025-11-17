cdb7c22df90a5f46a394d8570d784f68b19265cc
sujaynsv <sujaynsv@gmail.com>
2025-11-17 19:36:34 +0530

    Revert "updated the project code"
    This reverts commit 7602d5fe79c1d2c0e7e837183eb8dd43b566676f.

--- Documents/workspace-spring-tools-for-eclipse-4.32.2.RELEASE/Flight-Booking-System/src/main/java/com/flightapp/repository/AirlineRepository.java
+++
@@ -1,9 +0,0 @@
-package com.flightapp.repository;
-
-import com.flightapp.model.Airline;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AirlineRepository extends JpaRepository<Airline, Long> {
-}
--- Documents/workspace-spring-tools-for-eclipse-4.32.2.RELEASE/Flight-Booking-System/src/main/java/com/flightapp/repository/PassengerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.flightapp.repository;
-
-import com.flightapp.model.Passenger;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PassengerRepository extends JpaRepository<Passenger, Long> {
-}
